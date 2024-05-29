0ea15bda294622397c19d5ab015507591f68c461
Hamza Wahab Mirani <96471443+HamzaWindFury@users.noreply.github.com>
2024-05-29 09:14:16 +0000

    "One to one function is completed"

---
+++ one-to-one.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def is_one_to_one(func, dom):
+    set = []
+    for x in dom:
+        value = func(x)
+        if value in set:
+            return False
+        set.append(value)
+        print(f"f({x}) = {value}")
+    return True
+
+def plot_one_to_one(func, dom):
+    x = np.array(dom)
+    y = np.array([func(val) for val in dom])
+    plt.plot(x, y, marker = 'o', color = 'hotpink', ms = '6')  
+    font1 = {'fontsize': 15}
+    fon2 = {'fontsize': 20}
+    plt.xlabel('x', fontdict= font1)
+    plt.ylabel('f(x)', fontdict = font1)
+    plt.title('One-to-One Function', fontdict = font1)
+    plt.grid(axis = 'y')
+    plt.savefig('One to one.png')  
+
+def f(x):
+    return x + 1
+
+domain = [-1, -2, -3, -4, 1, 2, 3, 4]
+result = is_one_to_one(f, domain)
+print(f"The function is one-to-one: {result}")
+plot_one_to_one(f, domain)
